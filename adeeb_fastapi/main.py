3873c85770796ed7110a0a048e0b1b32b9f46eb2
m-shrief <m.shrief.dev@proton.me>
2026-05-17 10:21:13 +0300

    Updated CORS middleware.allow_origin

--- adeeb_fastapi/main.py
+++ adeeb_fastapi/main.py
@@ -36,7 +36,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "http://localhost",
+    "http://localhost:8000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
