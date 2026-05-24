53bd34c91b4bf258be246a87e5d4011eec91a5ab
m-shrief <m.shrief.dev@proton.me>
2026-05-24 18:32:16 +0300

    Added some reusable functions to decrease boilerplate while using the cache.

--- adeeb_fastapi/cache/index.py
+++ adeeb_fastapi/cache/index.py
@@ -29,3 +29,32 @@ async def get_async_cache()  -> AsyncGenerator[GlideClient, Any]:
         yield cache
     finally:
         await cache.close()
+
+# Reusable functions to reduce boilerplate
+
+async def cache_get(key: str, client: GlideClient):
+    try:
+        result = await client.get(key)
+        if result is None:
+            return None
+        obj: dict[str, Any] = loads(result.decode('utf-8'))
+        return obj
+    except Exception:
+        return None
+
+async def cache_set(key: str, value: BaseModel, client: GlideClient):
+    try:
+        _ = await client.set(
+            key=key,
+            value=value.model_dump_json(indent=0,exclude_none=True, exclude_unset=True), # set indent=0 to decrease the string size.
+            expiry=ExpirySet(ExpiryType.SEC, 60 * 15)
+        )
+    
+        return None
+    except Exception:
+        return None
+
+def format_key_by_id(prefix: str, id: UUID):
+    """Format key for a row in the database, using the individule name of the entity - adeebs --> adeeb,
+    and it's id"""
+    return f"{prefix}:{id}"
