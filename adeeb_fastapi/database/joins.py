5a1906f620ab049e82f8a8b1f70ad22d87431378
m.shrief <m.shrief.dev@proton.me>
2026-06-13 19:28:23 +0300

    Used selectinload instead of joinedload for Many-to-Many & One-to-Many relations.

--- adeeb_fastapi/database/joins.py
+++ adeeb_fastapi/database/joins.py
@@ -11,22 +11,24 @@ route():
 """
 
 from adeeb_fastapi.database.models import Adeeb as AdeebModel, Poem as PoemModel, ChosenVerses as ChosenVersesModel, ProseQoute as ProseQouteModel, Order as OrderModel, Print as PrintModel
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, selectinload
 
+# We use selectinload for One-to-Many & Many-to-Many relations -- like Users --> Orders
+# We use joinedload for One-to-One, Many-to-One relations or small collections -- like Poems --> Adeeb
 
 adeebs_to_poems = joinedload(PoemModel.adeeb).load_only(AdeebModel.id, AdeebModel.name)
 adeebs_to_chosen_verses = joinedload(ChosenVersesModel.adeeb).load_only(AdeebModel.id, AdeebModel.name)
 adeebs_to_prose_qoutes = joinedload(ProseQouteModel.adeeb).load_only(AdeebModel.id, AdeebModel.name)
 
-poems_to_adeeb = joinedload(AdeebModel.poems).load_only(PoemModel.id, PoemModel.intro)
+poems_to_adeeb = selectinload(AdeebModel.poems).load_only(PoemModel.id, PoemModel.intro)
 poems_to_chosen_verses = joinedload(ChosenVersesModel.poem).load_only(PoemModel.id, PoemModel.intro)
 
 chosen_verses_to_poem = joinedload(PoemModel.chosen_verses).load_only(ChosenVersesModel.id, ChosenVersesModel.verses)
-chosen_verses_to_adeeb = joinedload(AdeebModel.chosen_verses).load_only(ChosenVersesModel.id, ChosenVersesModel.verses)
+chosen_verses_to_adeeb = selectinload(AdeebModel.chosen_verses).load_only(ChosenVersesModel.id, ChosenVersesModel.verses)
 
-prose_qoutes_to_adeeb = joinedload(AdeebModel.prose_qoutes).load_only(ProseQouteModel.id, ProseQouteModel.qoute)
+prose_qoutes_to_adeeb = selectinload(AdeebModel.prose_qoutes).load_only(ProseQouteModel.id, ProseQouteModel.qoute)
 
 order_to_print = joinedload(PrintModel.order).load_only(OrderModel.id, OrderModel.name, OrderModel.user_id, OrderModel.delivery_schedule, OrderModel.is_updateable)
 
-# Here we get the Fullschema
-prints_to_order = joinedload(OrderModel.prints)#.load_only(PrintModel.id)
+# Fullschema for prints
+prints_to_order = selectinload(OrderModel.prints)#.load_only(PrintModel.id)
